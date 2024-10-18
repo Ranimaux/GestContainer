3deb3613755a9013583360a049f35b0496f075e4
Ranimaux <huon.jeje@gmail.com>
2024-10-18 16:09:35 +0200

    ez

---
+++ Gest-Container/Vues/FormFenetrePrincipale.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Gest_Container.Vues
+{
+    partial class FormFenetrePrincipale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormFenetrePrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(815, 450);
+            this.Name = "FormFenetrePrincipale";
+            this.RightToLeftLayout = true;
+            this.Text = "GestionContainer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
