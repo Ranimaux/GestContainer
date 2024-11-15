f634d24edc1ab4a04d8ecd2e594b31c50dd00ce4
Ranimaux <huon.jeje@gmail.com>
2024-11-15 15:12:02 +0100

    V0.1.6: Modify: add using MySql.data for all class job

--- Gest-Container/Modele/Declaration.cs
+++ Gest-Container/Modele/Declaration.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using GestContainer.Resources;
+using GestContainer.Modele;
+using GestContainer.Vues;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace GestContainer.Modele
 {
@@ -14,33 +20,21 @@ namespace GestContainer.Modele
         private bool _urgence;
         private bool _traite;
 
-        public int codeDeclaration
-        {
-            get { return _codeDeclaration; }
-        }
 
-        public string commentaireDeclaration
-        {
-            get { return _commentaireDeclaration; }
-        }
+        public int codeDeclaration { get; set; }
 
-        public DateTime date
-        {
-            get { return _date; }
-        }
+        public string commentaireDeclaration { get; set; }
 
-        public bool urgence
-        {
-            get { return _urgence; }
-        }
+        public DateTime dateDeclaration { get; set; }
 
-        public bool traite
-        {
-            get { return _traite; }
-        }
+        public bool urgence { get; set; }
+
+        public bool traite { get; set; }
         public Declaration()
         {
-
+            
         }
+
+        
     }
 }
